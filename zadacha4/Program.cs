f4ce9f1169663434ab27721b61b286d5406e7550
thenPie <118397975+thenPie@users.noreply.github.com>
2022-11-30 21:56:51 +0300

    did zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите три числа через пробел и я покажу max из них: ");
+
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int one = int.Parse(space[0]);
+int two = int.Parse(space[1]);
+int three = int.Parse(space[2]);
+int max = one;
+
+if (max < two) {
+    max = two;
+}
+if (max < three) {
+    max = three;
+}
+
+Console.WriteLine("max это — {0}", max);
