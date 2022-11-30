892f207dd7cf6ae56532594ec169db11af009a4b
thenPie <118397975+thenPie@users.noreply.github.com>
2022-11-30 22:18:18 +0300

    did zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число ");
+
+int isEven = int.Parse(Console.ReadLine());
+
+if (isEven % 2 == 0) {
+    Console.Write("Число {0} является чётным", isEven);
+} else {
+    Console.Write("Число не является чётным");
+}
