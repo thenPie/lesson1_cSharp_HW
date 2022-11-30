39adf379d94a649a6f0f5acd8cf14f3e08f3b8ad
thenPie <118397975+thenPie@users.noreply.github.com>
2022-11-30 21:23:35 +0300

    Initial commit, done zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите два числа и я покажу что больше, а что меньше: ");
+
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int a = int.Parse(space[0]);
+int b = int.Parse(space[1]);
+int min = a;
+int max = b;
+
+if (max < min) {
+    min = b;
+    max = a;
+}
+
+Console.WriteLine("min это — {0}, а max это — {1}", min, max);
